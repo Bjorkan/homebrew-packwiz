c1969904162109537a51ea977b8d1406ef7c1b12
Bjorkan <93771679+Bjorkan@users.noreply.github.com>
2025-10-05 00:58:51 +0200

    Remove spaces as requested from Homebrew styling

--- Formula/packwiz.rb
+++ Formula/packwiz.rb
@@ -2,7 +2,7 @@ class Packwiz < Formula
   desc "Command-line tool for creating, maintaining, and installing Minecraft modpacks"
   homepage "https://github.com/packwiz/packwiz"
   url "https://github.com/packwiz/packwiz/archive/adfe66935c410cdeebabbce702abe65fb1b90ddb.tar.gz"
-  version "0.0.0-adfe669"  # put version before sha256
+  version "0.0.0-adfe669" # version must come before sha256
   sha256 "f5f92f0ee72bf88bbc1a3beb23583611229b942e01119ff115d42fc0e6d7fa12"
   license "MIT"
   head "https://github.com/packwiz/packwiz.git", branch: "main"
@@ -10,7 +10,7 @@ class Packwiz < Formula
   depends_on "go" => :build
 
   def install
-    ldflags = %w[-s -w]  # %w not %W since no interpolation
+    ldflags = %w[-s -w] # use %w since no interpolation
     system "go", "build", *std_go_args(output: bin/"packwiz", ldflags: ldflags), "."
   end
 
