08f7a071e0d2fcc4a4a10c9212866eac841c9f6e
Bjorkan <93771679+Bjorkan@users.noreply.github.com>
2025-10-05 00:55:11 +0200

    Solved issues from Homebrew doctor

--- Formula/packwiz.rb
+++ Formula/packwiz.rb
@@ -2,19 +2,16 @@ class Packwiz < Formula
   desc "Command-line tool for creating, maintaining, and installing Minecraft modpacks"
   homepage "https://github.com/packwiz/packwiz"
   url "https://github.com/packwiz/packwiz/archive/adfe66935c410cdeebabbce702abe65fb1b90ddb.tar.gz"
+  version "0.0.0-adfe669"  # put version before sha256
   sha256 "f5f92f0ee72bf88bbc1a3beb23583611229b942e01119ff115d42fc0e6d7fa12"
-  version "0.0.0-adfe669"
   license "MIT"
   head "https://github.com/packwiz/packwiz.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    ldflags = %W[-s -w]
-    # Bygg från modulroten (INTE ./cmd/packwiz)
+    ldflags = %w[-s -w]  # %w not %W since no interpolation
     system "go", "build", *std_go_args(output: bin/"packwiz", ldflags: ldflags), "."
-    # (valfritt) generera completions om du vill:
-    # generate_completions_from_executable(bin/"packwiz", "completion")
   end
 
   test do
